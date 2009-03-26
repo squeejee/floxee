881ca616ff2cf1b8d489eb18f4fe6837c8500de6
Wynn Netherland <wynn@squeejee.com>
2009-03-25 22:23:26 -0500

    Memcached methods for application controller

--- init.rb
+++ init.rb
@@ -1,4 +1,19 @@
 require 'floxee'
 
 ActionView::Base.send :include, FloxeeHelper 
-ActionController::Base.send :include, Floxee::ControllerMethods 
+ActionController::Base.class_eval do 
+  
+  # gross hack for Memcached
+  before_filter :load_models
+
+  protected
+    def load_models
+      # gross hack for memcached
+      Person
+      TwitterStatus
+      Signature
+      Petition
+      TwitterUserStats
+    end
+  
+end
