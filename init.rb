be79b370e5e9604b07f572743b609261997150a9
Wynn Netherland <wynn@squeejee.com>
2009-03-25 22:18:50 -0500

    Moved app helper to floxee helper

--- init.rb
+++ init.rb
@@ -1,19 +1,4 @@
 require 'floxee'
 
-ApplicationContoller.class_eval do
-  
-  helper :floxee
-  
-  # gross hack for Memcached
-  before_filter :load_models
-  
-  protected
-    def load_models
-      # gross hack for memcached
-      Person
-      TwitterStatus
-      Signature
-      Petition
-      TwitterUserStats
-    end
-end
+ActionView::Base.send :include, FloxeeHelper 
+ActionController::Base.send :include, Floxee::ControllerMethods 
--- lib/floxee.rb
+++ lib/floxee.rb
@@ -10,4 +10,19 @@ module Floxee
   def self.server
     config['server'] || 'http://127.0.0.1:5984/floxee'    
   end
+  
+  module ControllerMethods
+    # gross hack for Memcached
+    before_filter :load_models
+
+    protected
+      def load_models
+        # gross hack for memcached
+        Person
+        TwitterStatus
+        Signature
+        Petition
+        TwitterUserStats
+      end
+  end
 end
