b4fc16497e186d47e4ecdf8e839165407228b87b
Wynn Netherland <wynn@squeejee.com>
2009-03-25 22:10:59 -0500

    Moved app helper to floxee helper

--- app/helpers/floxee_helper.rb
+++ app/helpers/floxee_helper.rb
@@ -1,5 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
-module ApplicationHelper
+module FloxeeHelper
   def twitter_auto_link(text)
     text = auto_link(text)
     # autolink @ tags reference people in Tweets
--- init.rb
+++ init.rb
@@ -1,1 +1,19 @@
 require 'floxee'
+
+ApplicationContoller.class_eval do
+  
+  helper :floxee
+  
+  # gross hack for Memcached
+  before_filter :load_models
+  
+  protected
+    def load_models
+      # gross hack for memcached
+      Person
+      TwitterStatus
+      Signature
+      Petition
+      TwitterUserStats
+    end
+end
