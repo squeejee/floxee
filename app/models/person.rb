686bcc5a54a702aa9d2ef7338855a3fe7e872b8d
Jason Derrett <jason@squeejee.com>
2009-04-09 18:29:15 -0500

    Fix person partial if no user...was bombing, fix screen_name problem

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,14 +5,21 @@ class Person < ActiveRecord::Base
   
   delegate :followers_count, :friends_count, :statuses_count, :profile_image_url, :description, :to => :user
   
+  validates_presence_of :first_name, :on => :create, :message => "can't be blank"
+  
   def display_name
     "#{nickname.blank? ? first_name : nickname} #{last_name}"
   end
   
   
   def screen_name=(value)
-    self.user = User.find_or_create_by_screen_name(value)
-    self.save
+    u = User.find_or_create_by_screen_name(value)
+    # The rescue clause in the function above returns
+    # "Could not retrieve user" if it doesn't find one, so check type
+    if u.is_a?(User)
+      self.user = u
+      self.save
+    end
   end
   
   def screen_name
